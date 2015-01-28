b24fa534aaeb666424cb1410936173086cf9eee1
Greg Hill <greg.hill@rackspace.com>
2015-01-28 09:55:55 -0600

    bump release number in preparation for release
    Should be releasing soon to get these bug fixes
    and improvements out to pypi.  Need to make this
    more automatic in the future.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ import setuptools
 
 setuptools.setup(
     name="python-ambariclient",
-    version=0.1,
+    version=0.2,
     author="Rackspace",
     author_email="greg.hill@rackspace.com",
     description="Client library for Apache Ambari.",
