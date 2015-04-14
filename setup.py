36dd2a9e5883745d5c96c131080aece15c1ceb03
Greg Hill <greg.hill@rackspace.com>
2015-04-14 10:39:40 -0500

    release 0.4.2

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open(requirements_path) as requirements_file:
 
 setuptools.setup(
     name="python-ambariclient",
-    version="0.4.1",
+    version="0.4.2",
     author="Rackspace",
     author_email="greg.hill@rackspace.com",
     description="Client library for Apache Ambari.",
